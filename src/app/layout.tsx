c276a1ac56bffe70dfc5e0e69e09f0c9cf443908
jswlee <jswleetaiwan@gmail.com>
2024-11-04 12:54:00 -1000

    Update layout.tsx
    Changed our font to Hind on layout page.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Hind } from 'next/font/google';
 import './globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from '@/components/Footer';
 import NavBar from '@/components/Navbar';
 import Providers from './providers';
 
-const inter = Inter({ subsets: ['latin'] });
+const hind = Hind({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'] });
 
 export const metadata: Metadata = {
   title: 'Aloha Archives',
@@ -18,7 +18,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const classString = `${inter.className} wrapper`;
+  const classString = `${hind.className} wrapper`;
   return (
     <html lang="en">
       <body className={classString}>
