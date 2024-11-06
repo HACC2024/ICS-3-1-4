db46d2fa33f99495483151ef59bb5b961a1e082d
jswlee <jswleetaiwan@gmail.com>
2024-11-06 11:44:33 -1000

    About Us Updated

--- src/app/info/about-us/page.tsx
+++ src/app/info/about-us/page.tsx
@@ -4,7 +4,11 @@ export default function AboutUs() {
       <h1>About Us</h1>
       <p>
         We are a team of students in the Computer Science department at the University of Hawaii.
-        Sharing a class called ICS 314, we named ourselves ICS 3+1=4, since we are Section 4 and
+        Organized and captained by JR Lee, the team members include Jared Seto, Shaelyn Loo, and
+        Kevin Clarkin. Sharing a class called ICS 314, we named ourselves ICS 3+1=4, since there
+        were 3 of us, we added 1 more, and we are Section 4. Only after countless hours (and some
+        head banging one the table) did we finally arrive at (the much catchier) Aloha Archives,
+        thanks to a moment of inspiration by Shaelyn. We have carried the name with us ever since.
       </p>
     </div>
   );
