5a6c2b78f663abfec3b6901a7ae4882628ea5ee2
jswlee <jswleetaiwan@gmail.com>
2024-11-06 11:13:57 -1000

    Update page.tsx
    About us text

--- src/app/info/about-us/page.tsx
+++ src/app/info/about-us/page.tsx
@@ -1,3 +1,11 @@
 export default function AboutUs() {
-  return <div>About Us Page Coming Soon</div>;
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        We are a team of students in the Computer Science department at the University of Hawaii.
+        Sharing a class called ICS 314, we named ourselves ICS 3+1=4, since we are Section 4 and
+      </p>
+    </div>
+  );
 }
