2480a522a3028846c532d5f0e3f2ed1235a084bd
Jared Seto <jseto2018@gmail.com>
2024-11-07 15:37:43 -1000

    Fixes #52

---
+++ src/components/NavSearchBar.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import React from 'react';
+import { Button, Container, Form, InputGroup } from 'react-bootstrap';
+import { Search } from 'react-bootstrap-icons';
+
+const SearchBar = () => {
+  // Read the search query from the URL when the component is first rendered
+  const urlParams = new URLSearchParams(window.location.search);
+  const initialQuery = urlParams.get('search') || ''; // Default to empty string if no search query in URL
+
+  const [query, setQuery] = React.useState(initialQuery);
+  const currentTopic = urlParams.get('topic') || ''; // Get current topic from URL
+
+  const handleSearch = () => {
+    // Construct the new URL with both search and topic params
+    const newUrl = `/results?search=${encodeURIComponent(query)}${currentTopic ? `&topic=${encodeURIComponent(currentTopic)}` : ''}`;
+
+    // Navigate to the new URL
+    window.location.href = newUrl;
+  };
+
+  const handleEnterPress = (event: { key: string }) => {
+    if (event.key === 'Enter') {
+      handleSearch(); // Trigger search on Enter key press
+    }
+  };
+
+  return (
+    <Container>
+      <InputGroup>
+        <Form.Control
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyDown={handleEnterPress}
+          placeholder="Search..."
+        />
+        <InputGroup.Text>
+          <Button id="searchIcon" onClick={handleSearch}>
+            <Search />
+          </Button>
+        </InputGroup.Text>
+      </InputGroup>
+    </Container>
+  );
+};
+
+export default SearchBar;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,8 +5,9 @@
 import { useSession } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
 import Image from 'next/image';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Col, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { BoxArrowRight, Lock, PersonCircle, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
+import NavSearchBar from './NavSearchBar';
 
 const NavBar: React.FC = () => {
   const { data: session } = useSession();
@@ -17,19 +18,19 @@ const NavBar: React.FC = () => {
   return (
     <Navbar className="custom-navbar" expand="lg">
       <Container>
-      <Navbar.Brand href="/">
-      <Image
-        src="/Seal_of_the_State_of_Hawaii.png"
-        alt="Hawaii State Seal"
-        width={100}
-        height={100}
-        className="d-inline-block align-top"
-      />
-      </Navbar.Brand>
+        <Navbar.Brand href="/">
+          <Image
+            src="/Seal_of_the_State_of_Hawaii.png"
+            alt="Hawaii State Seal"
+            width={100}
+            height={100}
+            className="d-inline-block align-top"
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto justify-content-start">
-          {/* {currentUser && role !== 'ADMIN' && (
+            {/* {currentUser && role !== 'ADMIN' && (
               <Nav.Link id="data-list" href="/list/data-list" key="list/data-list" active={pathName === '/list/data-list'}>
                 Data List
               </Nav.Link>
@@ -37,31 +38,36 @@ const NavBar: React.FC = () => {
             {currentUser
               ? [
                   <Nav.Link id="persona-stuff-nav" href="/persona" key="persona" active={pathName === '/persona'}>
-                  Take a Persona Quiz!
+                    Take a Persona Quiz!
                   </Nav.Link>,
                 ]
               : ''}
             {currentUser && role === 'ADMIN' ? (
-  <>
-  {/* <Nav.Link id="admin-stuff-nav" href="/admin" key="admin" active={pathName === '/admin'}>
+              <>
+                {/* <Nav.Link id="admin-stuff-nav" href="/admin" key="admin" active={pathName === '/admin'}>
                 Admin
   </Nav.Link> */}
-    <Nav.Link id="list-datasets-nav" href="/list/manage-data" key="list/manage-data" active={pathName === 'list/manage-data'}>
-      Manage Data
-    </Nav.Link>
-  </>
-) : (
-  ''
-)}
+                <Nav.Link id="list-datasets-nav" href="/list/manage-data" key="list/manage-data" active={pathName === 'list/manage-data'}>
+                  Manage Data
+                </Nav.Link>
+              </>
+            ) : (
+              ''
+            )}
           </Nav>
           {/* Right-aligned links */}
-          <Nav className="ms-auto">
+          <Nav className="ms-auto d-flex align-items-center">
             {currentUser && (
-              <Nav.Link id="profile-nav" href="/profile" active={pathName === '/profile'} className="d-flex align-items-center">
-                My Profile
-                {' '}
-                <PersonCircle size={20} className="ms-2" />
-              </Nav.Link>
+              <>
+                <Col>
+                  <NavSearchBar />
+                </Col>
+                <Nav.Link id="profile-nav" href="/profile" active={pathName === '/profile'} className="d-flex align-items-center">
+                  My Profile
+                  {' '}
+                  <PersonCircle size={20} className="ms-2" />
+                </Nav.Link>
+              </>
             )}
           </Nav>
           <Nav>
